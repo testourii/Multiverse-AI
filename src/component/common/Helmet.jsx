822b4b5aa6c0d6f47736fa381a6f75dda0162f84
houcem <houcem.testouri@we-settle.com>
2022-09-27 23:59:31 +0100

    cleaning code

--- src/component/common/Helmet.jsx
+++ src/component/common/Helmet.jsx
@@ -3,14 +3,17 @@ import {Helmet} from 'react-helmet'
 
 class PageHelmet extends Component{
     render(){
-        return(
-            <React.Fragment>
-                <Helmet>
-                    <title>{this.props.pageTitle} || React Multipurpose Template </title>
-                    <meta name="description" content="Trydo – Multipurpose React Template is a multi-use React template. It is designed to go well with multi-purpose websites. Trydo React Template will create that various landing Template, Creative Agency, And Corporate Website ." />
-                </Helmet>
-            </React.Fragment>
-        )
+        return (
+          <React.Fragment>
+            <Helmet>
+              <title>{this.props.pageTitle}</title>
+              <meta
+                name='description'
+                content='Multiverse AI || AI-Empowered Digital Transition towards Sustainable Development'
+              />
+            </Helmet>
+          </React.Fragment>
+        );
     }
 }
 
